80788c5f5fa47cdbd1c54d2b7e6104093b53b337
rchandra <rchandra@IN-PPM109465.ingrnet.com>
2023-06-20 18:33:32 +0530

    committing changes

--- my-java-springboot-app/src/main/java/hello/HelloController.java
+++ my-java-springboot-app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Spring Boot!";
+        return "Hello StarTrek from Spring Boot!";
     }   
 }
